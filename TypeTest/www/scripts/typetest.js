0edf8c80c6588584c7264a42a78aa039b36215ed
Aniket <allimaye@uwaterloo.ca>
2016-03-25 16:24:55 -0400

    Update 25/03/2016

--- TypeTest/www/scripts/typetest.js
+++ TypeTest/www/scripts/typetest.js
@@ -1,32 +1,7 @@
-﻿//// For an introduction to the Blank template, see the following documentation:
-//// http://go.microsoft.com/fwlink/?LinkID=397704
-//// To debug code on page load in Ripple or on Android devices/emulators: launch your app, set breakpoints, 
-//// and then run "window.location.reload()" in the JavaScript Console.
-//(function () {
-//    "use strict";
-
-//    document.addEventListener( 'deviceready', onDeviceReady.bind( this ), false );
-
-//    function onDeviceReady() {
-//        // Handle the Cordova pause and resume events
-//        document.addEventListener( 'pause', onPause.bind( this ), false );
-//        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-//        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-//    };
-
-//    function onPause() {
-//        // TODO: This application has been suspended. Save application state here.
-//    };
-
-//    function onResume() {
-//        // TODO: This application has been reactivated. Restore application state here.
-//    };
-//} )();
+﻿
 
 (function () {
 
-    //"use strict";
 
     var app = angular.module('typeTest', ['ngMaterial', 'ngMessages']);
 
@@ -37,50 +12,47 @@
 
     });
 
-    app.controller('InputController', function ($scope, $mdDialog) {
+    app.controller('InputController', function ($scope, $mdDialog, $interval, $filter) {
 
-        
-        var words = "first second third fourth fifth sixth seventh eighth ninth tenth";
+        var words = getRandomParagraph("sample_paragraphs.txt");
+        //var words = "first second third fourth fifth sixth seventh eighth ninth tenth";
         var wordSplit = words.split(' ');
         //$scope.paragraph = getRandomParagraph("sample_paragraphs.txt");
         $scope.paragraph = [];
-        $scope.totalChars = 0;
+        $scope.totalCorrectChars = 0;
         for (var i = 0; i < wordSplit.length; i++)
         {
             $scope.paragraph.push({ "value": wordSplit[i], "color": "black" });
-            $scope.totalChars += wordSplit[i].length;
+            $scope.totalCorrectChars += wordSplit[i].length;
         }
 
-        $scope.wordsPerChar = wordSplit.length / $scope.totalChars;
-        $scope.startTime = new Date();
-        $scope.currentTime = $scope.startTime;
+        $scope.wordsPerChar = wordSplit.length / $scope.totalCorrectChars;
+        $scope.wordSplitLength = wordSplit.length;
+        $scope.startTime = null;
+        $scope.currentTime = null;
+        //$scope.startTime = new Date();
+        //$scope.currentTime = $scope.startTime;
+        $scope.timerTime = "";
+        $scope.timerHandle = null;
 
         $scope.inputVal = "";
         $scope.debugVal = "";
-        $scope.determinateValue = 30;
         $scope.textToSpeech = false;
 
         $scope.currentWord = $scope.paragraph[0].value;
         $scope.counter = 0;
         $scope.wordCorrect = false;
         $scope.inputError = false;
-        $scope.charCount = 0;
-
+        $scope.correctChars = 0;
+        $scope.typedChars = 0;
+        $scope.progress = $scope.correctChars / $scope.totalCorrectChars;
 
         $scope.speed = 20;
         $scope.accuracy = 90;
-        $scope.precision = 70;
-
-
-
-        
-
-        
-
 
         $scope.$watch('inputVal', function (newVal, oldVal) {
 
-            //if (newVal === oldVal) return;
+            
 
             var lastChar = newVal.charAt(newVal.length - 1);
 
@@ -92,9 +64,20 @@
 
                 if ($scope.wordCorrect)
                 {
+                    //indicate visually that the word has been spelt correctly
                     $scope.paragraph[$scope.counter].color = "green";
+                    //move on to the next word
                     $scope.counter++;
+
+                    var isLastWord = $scope.counter === $scope.wordSplitLength;
+                    if (isLastWord)
+                    {
+                        endOfTest();
+                        return;
+                    }
+
                     $scope.currentWord = $scope.paragraph[$scope.counter].value;
+                    //reset input text box
                     $scope.inputVal = "";
                     $scope.wordCorrect = false;
                     if ($scope.textToSpeech)
@@ -112,50 +95,34 @@
             //Case 2: Last character entered was not a space
             else
             {
+                //record that a new non-space character has been typed (regardless of whether it is correct or not)
+                $scope.typedChars += newVal.length > oldVal.length ? 1 : 0;
+
                 //determine if the current word is being spelt correctly and show error if not
                 $scope.inputError = newVal != $scope.currentWord.substring(0, newVal.length);
                 
                 //increment charcter count as long as last change is not a deletion of letters (false positive), and it is correct
-                $scope.charCount += $scope.inputError || newVal.length < oldVal.length ? 0 : 1;
+                $scope.correctChars += $scope.inputError || newVal.length < oldVal.length ? 0 : 1;
             }
 
 
-            //$scope.debugVal = "newVal: " + newVal + ", oldVal: " + oldVal + ", charCount: " + $scope.charCount;
+            //$scope.debugVal = "newVal: " + newVal + ", oldVal: " + oldVal + ", correctChars: " + $scope.correctChars;
             $scope.debugVal = "startTime" + $scope.startTime + ", currentTime "+ $scope.currentTime;
 
             updateUIColors($scope.inputError)
             updateStatistics();
+            updateProgressBar();
             
 
         });
 
 
-        //$(document).ready(function () {
-        //    $("#paragraphInput").on("keypress", function (eventData) {
-               
-        //        //detect that the last character entered was a space and then do a validation
-        //        if(eventData.originalEvent.code == "Space")
-        //        {
-        //            //validate the word
-        //            $scope.wordCorrect = $("#paragraphInput").val().trim() === $scope.currentWord;
-        //            if ($scope.wordCorrect)
-        //            {
-        //                $scope.counter++;
-        //                $scope.currentWord = $scope.paragraph[$scope.counter][0];
-        //                $("#paragraphInput").val("");
-        //            }
-        //            else
-        //            {
-        //                //the user is trying to move onto a new word whilst having spelt the current word incorrectly
-        //            }
-        //        }
-        //    });
-        //});
 
+        
 
         function updateProgressBar()
         {
-
+            $scope.progress = ($scope.counter / $scope.wordSplitLength) * 100;
         }
         
         function updateStatistics()
@@ -163,13 +130,26 @@
             //update typing speed
             $scope.currentTime = new Date();
             var timeDiffInMins = ($scope.currentTime.getTime() - $scope.startTime.getTime()) / 1000 / 60;
-            var charsPerMin = $scope.charCount / timeDiffInMins;
+            var charsPerMin = $scope.correctChars / timeDiffInMins;
             var wpm = $scope.wordsPerChar * charsPerMin;
             $scope.speed = Math.round(wpm);
-            
-            //update precision
 
             //update accuracy
+            $scope.accuracy = Math.round(($scope.correctChars / $scope.typedChars) * 100);
+            
+
+        }
+
+        function updateTimer()
+        {
+            //var milliseconds = (new Date().getTime() - $scope.startTime.getTime());
+            //var seconds = (milliseconds / 1000);
+            //var minutes = seconds / 60;
+            //$scope.timerTime = ("0" + Math.round(minutes)).slice(-2) + ":" +
+            //                        ("0" + Math.round(seconds)).slice(-2) + ":" +
+            //                        ("0" + Math.round(milliseconds)).slice(-3);
+
+            $scope.timerTime = $filter('date')(new Date().getTime() - $scope.startTime.getTime(), "mm:ss:sss");
         }
 
         function updateUIColors(inputError)
@@ -191,99 +171,110 @@
             }
         }
 
-        $scope.showTestDialog = function () {
-            //$mdDialog.show(
-            //  $mdDialog.alert()
-            //    .clickOutsideToClose(true)
-            //    .title('Opening from offscreen')
-            //    .textContent('Closing to offscreen')
-            //    .ariaLabel('Offscreen Demo')
-            //    .ok('Amazing!')
-            //    // Or you can specify the rect to do the transition from
-            //    .openFrom(
-            //    {
-            //        top: -50,
-            //        width: 30,
-            //        height: 80
-            //    })
-            //    .closeTo(
-            //    {
-            //        left: 1500
-            //    })
-            //);
+        function getRandomParagraph(file) {
 
+            var fileText = "";
+            var rawFile = new XMLHttpRequest();
+            rawFile.open("GET", file, false);
+            rawFile.onreadystatechange = function () {
+                if (rawFile.readyState === 4) {
+                    if (rawFile.status === 200 || rawFile.status == 0) {
+                        fileText = rawFile.responseText;
+                    }
+                }
+            }
+            rawFile.send(null);
+
+            
+
+            //select a random paragraph from the text file
+            var random = Math.floor((Math.random() * 3));
+
+            var allText = fileText.split('%')[random].trim();
+
+            $scope.paragraphTitle = allText.split('>')[0].trim();
+
+            return allText.split('>')[1].trim();
+        }
+
+        function endOfTest()
+        {
+            $scope.counter++;
+        }
+
+
+        $scope.showInstructionDialog = function () {
+
+            $mdDialog.show({
+                scope: $scope,        // use parent scope in template
+                preserveScope: true,
+                templateUrl: 'Instructions.html',
+                openFrom: {
+                    top: -50,
+                    width: 30,
+                    height: 80
+                },
+                closeTo: {
+                    left: 1500
+                },
+                controller: function DialogController($scope, $mdDialog) {
+                    $scope.moveToUserInfo = function () {
+                        $mdDialog.templateUrl = 'UserInfo.html';
+                    };
+
+                    $scope.closeInstructionDialog = function () {
+                        $mdDialog.hide();
+                        $scope.showUserInfoDialog();
+                    };
+
+                    $scope.closeUserInfoDialog = function () {
+                        $mdDialog.hide();
+                        $scope.showTrafficLightAnimation();
+                    };
+                }
+            });
+
+        };
+
+        $scope.showUserInfoDialog = function () {
 
             $mdDialog.show({
                 scope: $scope,        // use parent scope in template
                 preserveScope: true,
                 templateUrl: 'UserInfo.html',
-                onComplete: afterShowAnimation
+                
+            }).then(function () {
+                $("#paragraphInput").focus();
+            });
+        };
+        
+        $scope.showTrafficLightAnimation = function ()
+        {
+            $mdDialog.show({
+                template:
+                  '<md-dialog layout-wrap>' +
+                  //'     <md-dialog-content>' +
+                  '         <center><img src="../images/readySteadyGo.gif" height="232" width="122"/><center>' +
+                  //'     </md-dialog-content>' +
+                  '</md-dialog>',
+                onComplete: afterShowAnimation,
+                autoWrap: false
             });
 
+            
             // When the 'enter' animation finishes...
             function afterShowAnimation(scope, element, options) {
-                // post-show code here: DOM element focus, etc.
+                setTimeout(function () {
+                    $mdDialog.hide();
+                    $scope.startTime = new Date();
+                    $scope.currentTime = new Date();
+                    $scope.timerHandle = $interval(updateTimer, 10);
+                }, 4500);
             }
 
-            $scope.closeDialog = function () {
-                $mdDialog.hide();
-            };
-           
-
-
-        };
-
-       
-
-        function getRandomParagraph(file) {
-
-            //var rawFile = new XMLHttpRequest();
-            //rawFile.open("GET", file, false);
-            //rawFile.onreadystatechange = function () {
-            //    if (rawFile.readyState === 4) {
-            //        if (rawFile.status === 200 || rawFile.status == 0) {
-            //            var allText = rawFile.responseText;
-            //            alert(allText);
-            //        }
-            //    }
-            //}
-            //rawFile.send(null);
-            //return rawFile.responseText;
-
-            var paragraph = "Whether you're a student, secretary, office administrator, manager, computer programmer, or engineer, typing " +
-            "is an invaluable skill. It's a skill that can open doors and enhance your career opportunities.";
-
-            return paragraph.split(" ");
         }
 
-
-       
-        $mdDialog.show({
-            scope: $scope,        // use parent scope in template
-            preserveScope: true,
-            templateUrl: 'Instructions.html',
-            openFrom: {
-                top: -50,
-                width: 30,
-                height: 80
-            },
-            closeTo: {
-                left: 1500
-            },
-            onComplete: $scope.showTestDialog
-        }).then(function (response) {
-            if (response == 'ok') {
-                $mdDialog.hide();
-            }
-        });
-
-        $(document).ready(function () {
-
-            
-
-        });
-
-
+        $scope.showInstructionDialog();
 
     });
 
@@ -306,6 +297,13 @@
     {
         type_speeds.push(i);
     }
+
+
+
+
+    //$scope.$on('$viewContentLoaded', function () {
+    //    var x = 2;
+    //});
     
 
 
