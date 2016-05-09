25abda668ec66ab70695346f20196287e983eef6
Aniket Limaye <allimaye@uwaterloo.ca>
2016-05-08 20:32:53 -0400

    Restructured directories and added a server directory to prepare for deployment to Heroku.

---
+++ server/app.js
@@ -0,0 +1,23 @@
+﻿
+    var express = require('express');
+    var app = express();
+    var logger = require('morgan');
+
+    //Use Morgan for logging
+    app.use(logger('dev'));
+
+    //Middleware (allow cross origin requests)
+    app.use(function (req, res, next) { //allow cross origin requests
+        res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
+        res.header("Access-Control-Allow-Origin", "http://localhost");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
+
+    app.use(express.static(process.cwd() + '/client'));
+
+    var port = Number(process.env.PORT || '7070');
+
+    app.listen(port, function () {
+        console.log('running on port ' + port + '...');
+    });
